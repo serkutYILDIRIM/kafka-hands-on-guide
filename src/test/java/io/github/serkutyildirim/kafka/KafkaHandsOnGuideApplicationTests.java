3cc65d70af86470a84493fafda178383c6af5562
serkutYILDIRIM <serkutyldrm@gmail.com>
2026-01-22 21:04:14 +0300

    Initial commit

---
+++ src/test/java/io/github/serkutyildirim/kafka/KafkaHandsOnGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.serkutyildirim.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaHandsOnGuideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
